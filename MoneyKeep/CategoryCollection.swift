cb5ca665e908505d17d3ccb7dc63fff0153a0111
Albert <rubberbigpepper@gmail.com>
2020-08-28 16:50:52 +0500

    Разделил категории и траты по разным классам. Поправил запись в коредату

---
+++ MoneyKeep/CategoryCollection.swift
@@ -0,0 +1,121 @@
+//
+//  CategoryCollection.swift
+//  MoneyKeep
+//
+//  Created by Albert on 28.08.2020.
+//  Copyright © 2020 Albert. All rights reserved.
+//
+
+import Foundation
+import CoreData
+import UIKit
+
+class CategoryCollection{
+    public static let Categories: CategoryCollection = CategoryCollection() //реализуем синглтон
+
+    private var categories: [SpendCategory] = []//все категории расходов будут тут
+    
+    private var persistentsContainer: NSPersistentContainer{
+        get{
+           let container = NSPersistentContainer(name: "SavingLearn")
+            container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+                if let error = error as NSError? {
+                    fatalError("Unresolved error \(error), \(error.userInfo)")
+                }
+            })
+            return container
+        }
+    }
+
+    public var count: Int{//количество категорий
+        get{
+            return categories.count
+        }
+    }
+    
+    private init(){
+        
+    }
+    
+    public func addCatehory(_ category: SpendCategory){//добавление категории
+        if FindCategory(category.ID) != nil { return }//такая категория уже есть, пропускаем
+        categories.append(category)
+    }
+    
+    public func getCategory(_ at: Int)->SpendCategory?{//доступ к категории по индексу
+        if at<0 || at >= count {  return nil }
+        return categories[at]
+    }
+    
+    public func removeCategoryAt(_ at: Int){//удаление категории
+        if at<0 || at >= count {  return }
+        categories.remove(at: at)
+    }
+    
+    public func removeCategory(_ ID: Int){//удаление категории
+        let at: Int = FindCategory(ID)
+        if at<0 { return }
+        categories.remove(at: at)
+    }
+        
+    private func FindCategory(_ catID: Int)->Int{//поищем категорию с указанным ID
+        for (idx, category) in self.categories.enumerated(){
+            if category.ID==catID { return idx }
+        }
+        return -1
+    }
+    
+    public func FindCategory(_ catID: Int)->SpendCategory?{//поищем категорию с указанным ID, если нет - nil
+        return getCategory(FindCategory(catID))
+    }
+    
+    
+    public func SaveToCoredata() {
+        ClearCoredata()
+        let persistentContainer = persistentsContainer
+        let managedContext = persistentContainer.viewContext
+        
+        for category in categories{//сохраняем категории
+            let entity =  NSEntityDescription.entity(forEntityName: "CategoryEntity", in: managedContext)
+            let item = NSManagedObject(entity: entity!, insertInto:managedContext)
+         
+            item.setValue(category.ID, forKey: "cat_id")
+            item.setValue(category.name, forKey: "name")
+            item.setValue(category.imagePath, forKey: "icon")
+        }
+    }
+    
+    public func ReadFromCoredata(){//чтение категорий
+        categories.removeAll()
+        let persistentContainer = persistentsContainer
+        let managedContext = persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"CategoryEntity")
+        let fetchedResults = try!managedContext.fetch(fetchRequest) as? [NSManagedObject]
+
+        if let results = fetchedResults {
+            for result in results{
+                let category = SpendCategory(imagePath: result.value(forKey: "icon") as! String,
+                                             name: result.value(forKey: "name") as! String,
+                                             ID: result.value(forKey: "cat_id") as! Int)
+                categories.append(category)
+            }
+        }
+    }
+    
+    private func ClearCoredata(){
+        let persistentContainer = persistentsContainer
+        let managedContext = persistentContainer.viewContext
+
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"CategoryEntity")
+       
+        let fetchedResults = try!managedContext.fetch(fetchRequest) as? [NSManagedObject]
+
+        if let results = fetchedResults {
+            for result in results{
+                result.prepareForDeletion()
+            }
+        }
+    }
+        
+}
--- MoneyKeep/SpendCollection.swift
+++ MoneyKeep/SpendCollection.swift
@@ -12,42 +12,46 @@ import UIKit
 	
 class SpendCollection{//тут будет полный список расходов
     private var spends: [SpendItem] = []//все расходы будем писать тут
-    private var categories: [SpendCategory] = []//все категории расходов будут тут
     
-    public var count: Int{//количество итемов
-        return spends.count
-    }
-    
-    init() {//конструктор, получим данные из CoreData
-
-    }
-    
-    deinit {//а тут сохраним в CoreData
-        
-    }
-    
-    func addItem(_ item: SpendItem){//добавление расхода в коллекцию
-        spends.append(item)
-        spends.sort { (a: SpendItem, b: SpendItem) -> Bool in //сразу отсортируем коллекцию по возрастанию
-            return a.date < b.date
-        }
-    }
-    
-    func getItem(_ at: Int)->SpendItem?{//доступ к расходу по индексу
-        if at<0 || at >= count {  return nil }
-        return spends[at]
-    }
-    
-    public func SaveToCoredata() {
-        let persistentContainer: NSPersistentContainer = {
-            let container = NSPersistentContainer(name: "SavingLearn")
+    private var persistentsContainer: NSPersistentContainer{
+        get{
+           let container = NSPersistentContainer(name: "SavingLearn")
             container.loadPersistentStores(completionHandler: { (storeDescription, error) in
                 if let error = error as NSError? {
                     fatalError("Unresolved error \(error), \(error.userInfo)")
                 }
             })
             return container
-        }()
+        }
+    }
+    
+    public var count: Int{//количество итемов
+        return spends.count
+    }
+    
+    init() {//конструктор, получим данные из CoreData
+        ReadFromCoredata()
+    }
+    
+    deinit {//а тут сохраним в CoreData
+        SaveToCoredata()
+    }
+    
+    public func addItem(_ item: SpendItem){//добавление расхода в коллекцию
+        spends.append(item)
+        spends.sort { (a: SpendItem, b: SpendItem) -> Bool in //сразу отсортируем коллекцию по возрастанию
+            return a.date < b.date
+        }
+    }
+    
+    public func getItem(_ at: Int)->SpendItem?{//доступ к расходу по индексу
+        if at<0 || at >= count {  return nil }
+        return spends[at]
+    }
+    
+    public func SaveToCoredata() {
+        ClearCoredata()
+        let persistentContainer = persistentsContainer
         let managedContext = persistentContainer.viewContext
         
         for spendItem in spends{//сохраняем итемы
@@ -59,38 +63,43 @@ class SpendCollection{//тут будет полный список расход
             item.setValue(spendItem.text, forKey: "text")
             item.setValue(spendItem.category.ID, forKey: "cat_id")
         }
-        
-        for category in categories{//сохраняем категории
-            let entity =  NSEntityDescription.entity(forEntityName: "CategoryEntity", in: managedContext)
-            let item = NSManagedObject(entity: entity!, insertInto:managedContext)
-         
-            item.setValue(category.ID, forKey: "cat_id")
-            item.setValue(category.name, forKey: "name")
-            item.setValue(category.imagePath, forKey: "icon")
-        }
+        CategoryCollection.Categories.SaveToCoredata()
     }
     
     public func ReadFromCoredata(){
-        let persistentContainer: NSPersistentContainer = {
-                 let container = NSPersistentContainer(name: "SavingLearn")
-                 container.loadPersistentStores(completionHandler: { (storeDescription, error) in
-                     if let error = error as NSError? {
-                         fatalError("Unresolved error \(error), \(error.userInfo)")
-                     }
-                 })
-                 return container
-             }()
+        CategoryCollection.Categories.ReadFromCoredata()
+        let persistentContainer = persistentsContainer
+        let managedContext = persistentContainer.viewContext
+        spends.removeAll()
+        
+        let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"SpendsEntity")
+        let fetchedResults = try!managedContext.fetch(fetchRequest) as? [NSManagedObject]
+
+        if let results = fetchedResults {//читаем по одному итемы
+            for result in results{
+                let catID = result.value(forKey: "cat_id") as! Int
+                let category = CategoryCollection.Categories.FindCategory(catID) ?? SpendCategory() //сопоставляем класс категории трат
+                let spendItem = SpendItem(category: category,
+                                          amount: result.value(forKey: "amount") as! Float,
+                                          date: result.value(forKey: "date") as! Date,
+                                          text: result.value(forKey: "text") as! String)
+                spends.append(spendItem)
+            }
+         }
+    }
+    
+    private func ClearCoredata(){
+        let persistentContainer = persistentsContainer
         let managedContext = persistentContainer.viewContext
 
         let fetchRequest = NSFetchRequest<NSFetchRequestResult>(entityName:"SpendsEntity")
-        
+       
         let fetchedResults = try!managedContext.fetch(fetchRequest) as? [NSManagedObject]
-        todoList.removeAll()
+
         if let results = fetchedResults {
             for result in results{
-                let todo=Todo(what: result.value(forKey: "what") as! String, when: result.value(forKey: "when") as! Date)
-                todoList.append(todo)
+                result.prepareForDeletion()
             }
-         }
+        }
     }
 }
