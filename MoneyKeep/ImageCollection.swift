db92723fa1ab1af32dd3a3e089b6b5857cc6f260
Albert <rubberbigpepper@gmail.com>
2020-09-01 16:23:21 +0500

    правки от 01.09.2020

--- MoneyKeep/ImageCollection.swift
+++ MoneyKeep/ImageCollection.swift
@@ -11,39 +11,36 @@ import UIKit
 //класс, содержащий битмапы для отрисовки
 class ImageCollection{
     var images: [String: UIImage] = [:]//главный словарь
-    var mostUsage : [String]=[] //наиболее часто используемые картинки, поставим их в начало
     
     public var count: Int {//количество картинок в коллекции
         get{
-            return mostUsage.count
+            return images.count
         }
     }
     
-    public func getImage(_ name: String)->UIImage?{
-        moveToTop(name)
+    public func addImage(_ name: String){
+        removeImage(name)
+        images[name]=UIImage(named: name)
+    }
+    
+    public func removeImage(_ name: String){
+        images.removeValue(forKey: name)
+    }
+    
+    public func getImage(_ name: String, createNew: Bool = false)->UIImage?{
+        if images.index(forKey: name) == nil {//такого еще нет
+            if createNew {//создаем новый как указано
+                addImage(name)
+                return getImage(name, createNew: false)
+            }
+            else{
+                return nil
+            }
+        }
         return images[name]
     }
     
-    public func getImage(_ at: Int)->UIImage?{
-        if at<0 || at>=count { return nil }
-        let name=mostUsage[at]
-        moveToTop(name)
-        return images[name]
-    }
-    
-    public func getImageName(_ at: Int)->String?{
-        if at<0 || at>=count { return nil }
-        return mostUsage[at]
-    }
-    
-    private func moveToTop(_ name: String){
-        mostUsage.removeAll(where: {$0==name})
-        mostUsage.insert(name, at: 0)
-    }
-    
-    private func moveToTop(_ at: Int){
-        let name=mostUsage[at]
-        mostUsage.remove(at: at)
-        mostUsage.insert(name, at: 0)
+    public func Clear(){//очистка коллекции
+        images.removeAll()
     }
 }
